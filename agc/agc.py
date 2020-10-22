085c42c81b391e6a9c18e01a2b0bea7ee39f9fd4
Julien Pichon <julien.pichon@cri-paris.org>
2020-10-22 16:18:00 +0200

    until search_mates function

--- agc/agc.py
+++ agc/agc.py
@@ -68,6 +68,78 @@ def get_arguments():
     parser.add_argument('-o', '-output_file', dest='output_file', type=str,
                         default="OTU.fasta", help="Output file")
     return parser.parse_args()
+    
+    
+def read_fasta(amplicon_file, minseqlen):
+    """Take a fasta file and a minimum sequence length as argument.
+        Return a generator of sequence with the length filtered.
+    """"
+    with gzip.open(amplicon_file, "r") as filin:
+        for line in filin:
+            if len(line) >= minseqlen:
+                yield line.strip()
+
+
+def dereplication_fulllength(amplicon_file, minseqlen, mincount):
+    """Take a generator of sequence, a minimum sequence length and a minimum number of
+        occurence as arguments.
+        Return a generator of sequence with at least 40 occurences
+    """
+    occurence_dict = {}
+    for seq in read_fasta(amplicon_file, minseqlen):
+        if seq not in occurence_dict:
+            occurence_dict[seq] = 1
+        else:
+            occurence_dict[seq] += 1
+    for seq, nb_seq in sorted(occurence_dict.items(), key = lambda item: item[1], reverse = True):
+        if nb_seq < 40:
+        	break
+        else:
+        	yield [seq, nb_seq]
+        	
+        	
+def get_chunks(sequence, chunk_size):
+    """Take a sequence and a size as arguments.
+        Cut the sequence in the correct number of chunks if there is at least
+        four chunks returned.
+    """
+    chunks = []
+    if len(sequence) < 4 * chunk_size:
+        pass
+    else:
+        if len(sequence) % chunk_size == 0:
+            nb_chunks = len(sequence) / chunk_size
+            for chunk in range(nb_chunks - 1):
+                chunks.append[sequence[chunk * chunk_size:(chunk + 1) * chunk_size]]
+        else:
+            nb_chunks = len(sequence) / chunk_size
+            for chunk in range(nb_chunks - 2):
+                chunks.append[sequence[chunk * chunk_size:(chunk + 1) * chunk_size]]
+    return chunks
+
+
+def cut_kmer(sequence, kmer_size):
+    """Take a sequence and a siez of kmer as arguments.
+        Return all the possible kmer as generator.
+    """
+    for i in range(len(sequence) - kmer_size + 1):
+        yield sequence[i:i+kmer_size]
+        
+        
+def get_unique_kmer(kmer_dict, sequence, id_seq, kmer_size):
+    """Take a dictionnary with a kmer as index and a list of id as values.
+    """
+    for kmer in cut_kmer(sequence, kmer_size):
+        if kmer not in kmer_dict:
+            kmer_dict[kmer] = []
+        kmer_dict[kmer].append(id_seq)
+    return kmer_dict
+
+
+def search_mates(kmer_dict, sequence, kmer_size):
+    return [i[0] for i in Counter([ids for kmer in cut_kmer(sequence, km_size) 
+        if kmer in kmer_dict for ids in kmer_dict[kmer]]).most_common(8)]
+
 
 #==============================================================
 # Main program
@@ -78,6 +150,8 @@ def main():
     """
     # Get arguments
     args = get_arguments()
+    get_chunks(args.amplicon_file, args.minseqlen, args.mincount, args.chunk_size)
+    
 
 
 if __name__ == '__main__':
